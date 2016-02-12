eed95acff769eac51334b3348478565ea91b96a1
David Lepaux <d.lepaux@gmail.com>
2016-02-12 21:11:38 +0100

    uncomment  define mithril route

--- app/isomorphic/client/index.js
+++ app/isomorphic/client/index.js
@@ -14,6 +14,6 @@ var routes = require('./../../routes');
 // Define route style, (pathname|hash|search)
 m.route.mode = 'pathname';
 // Define routes when DOM Ready
-/*domready(function() {
+domready(function() {
   m.route(document.body , '/', routes);
-});*/
+});
