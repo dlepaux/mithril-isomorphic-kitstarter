a5e8452d6f04d3678323a64c3d37abc1471400a5
David Lepaux <d.lepaux@gmail.com>
2016-03-02 16:56:53 +0100

    blade lib

--- app/lib/blade.js
+++ app/lib/blade.js
@@ -5,26 +5,26 @@ var path   = require('path');
 var fs     = require('fs');
 
 function layout(data) {
-	console.log(path.join(__dirname, './../views/layout.html'));
-	fs.readFileSync(path.join(__dirname, '../views/layout.html'), 'utf8', (err, string) => {
+	return fs.readFileSync(path.join(__dirname, './../views/layout.html'), 'utf8', function (err, string) {
 	  	if (err) {
 	  		console.log(err);
 	  	}
-		console.log(data);
 		var matchEscape = string.match(/\{\{\{[\ ]*[a-zA-Z]+[\ ]*\}\}\}/g);
 		var match = string.match(/\{\{[\ ]*[a-zA-Z]+[\ ]*\}\}/g);
 		
 		if (matchEscape) {
+			console.log('HELOOOOOOOOOOOOOOOOOOOOO');
 			for (var o in data) {
 				string = string.replace(match, escape(data[o]));
 			}
 		} else if (match) {
+			console.log('HELOOOOOOOOOOOOOOOOOOOOO');
 			// show data
 			for (var o in data) {
 				string = string.replace(match, data[o]);
 			}
 		}
-		console.log(string);
+		console.log('HELOOOOOOOOOOOOOOOOOOOOO');
 		return string;
 	});
 }
