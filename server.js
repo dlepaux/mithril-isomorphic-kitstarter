0ae0082ac669daee10a13309304618fdf69bcf07
David Lepaux <d.lepaux@gmail.com>
2016-03-10 19:45:40 +0100

    add request and test on /api

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var web 		= require('./dist/api/web');
 var rest 		= require('./dist/api/rest');
 var path   	 	= require('path');
 var compression = require('compression');
+var request     = require('request');
 
 // Init Server
 var app = express();
@@ -23,6 +24,20 @@ app.use(compression());
 app.use(web);
 // API
 app.use('/api/v1', rest);
+
+// Proxy
+app.use('/api', function(req, res) {
+  var url = 'http://dynam.ix/api/i18n/2'+ req.url;
+  console.log(url);
+  var r = null;
+  if(req.method === 'POST') {
+    r = request.post({uri: url, json: req.body});
+  } else {
+    r = request(url);
+  }
+  console.log(r);
+  req.pipe(r).pipe(res);
+});
 
 // Join public rep to use css, img, etc
 app.use(express.static(path.join(__dirname, 'public')));
